30591c6d12cdf81b85bcb0d8c35d53e1f9b329d2
Douglas Melvin <dmelvin@kenzanmedia.com>
2016-04-27 01:12:02 +0000

    Admin and model changes to add full admin interface

--- room_calendar/admin.py
+++ room_calendar/admin.py
@@ -2,12 +2,21 @@ from django.contrib import admin
 
 from django.contrib import admin
 
-from .models import Room, OfficeLocation
+from .models import Room, OfficeLocation, Reservation
 
 class RoomInline(admin.TabularInline):
     fields = ['room_name_long', 'room_name_short', 'room_mailbox']
     model = Room
     extra = 1
+
+class ReservationAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Meeting Information',{'fields' : ['reservation_title','reservation_room']}),
+        ('Meeting Attendees',{'fields' : ['reservation_owner','reservation_attendees']}),
+        ('Meeting Duration',{'fields' : ['reservation_start','reservation_end']}),
+        ('Exchange Data',{'fields' : ['reservation_id']}),
+    ]
+admin.site.register(Reservation, ReservationAdmin)
 
 class OfficeLocationAdmin(admin.ModelAdmin):
     fieldsets = [
